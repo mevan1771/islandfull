2b34832f8d35791852b53c7f6dff04bb63d2a5a6
mevan1771 <mevan1771@gmail.com>
2026-08-19 00:48:22 +0100

    ui: update ActivityCard aspect ratio to square for a more compact grid

--- components/activity/ActivityCard.tsx
+++ components/activity/ActivityCard.tsx
@@ -74,7 +74,7 @@ export function ActivityCard({
     <Link href={`/activity/${slug}`} prefetch={true} className="block group h-full">
       <div className="flex flex-col gap-2 h-full">
         {/* Image / Video Container */}
-        <div className="relative aspect-[4/3] sm:aspect-[3/4] w-full overflow-hidden rounded-xl md:rounded-3xl bg-zinc-100">
+        <div className="relative aspect-square w-full overflow-hidden rounded-xl md:rounded-3xl bg-zinc-100">
           {videoUrl ? (
             <video
               ref={videoRef}
