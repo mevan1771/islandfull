2027809614ba0e65ce1cfa20c1fbe053eba75a57
mevan1771 <mevan1771@gmail.com>
2026-08-21 20:57:33 +0100

    fix: apply root level structural fix to pin footer to bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,12 +39,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="antialiased">
+    <html lang="en" className="h-full antialiased">
       <body className={`${inter.className} bg-white min-h-screen flex flex-col overflow-x-hidden`}>
         <Toaster position="top-center" />
         <SiteHeader />
 
-        <main className="flex-1 flex flex-col w-full">
+        <main className="flex-grow">
           {children}
         </main>
 
