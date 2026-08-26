b5639aab8f42f9c68d44d09ca14fb58444b5b07a
mevan1771 <mevan1771@gmail.com>
2026-08-26 20:52:15 +0100

    style: fix homepage hero overlay to be vibrant

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -71,8 +71,8 @@ export function HeroCarousel({ tours, introSlide }: { tours: Tour[], introSlide?
           />
 
           {/* Slide-specific Legibility Mask */}
-          <div className="absolute bottom-0 left-0 right-0 h-[40%] bg-gradient-to-t from-black/50 via-transparent to-transparent pointer-events-none"></div>
-          <div className="absolute inset-x-0 top-0 h-24 bg-gradient-to-b from-black/40 via-transparent to-transparent pointer-events-none"></div>
+          <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/5 to-transparent pointer-events-none z-10"></div>
+          <div className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-black/40 to-transparent pointer-events-none z-10"></div>
 
           {/* Slide Content */}
           <div className="absolute bottom-8 md:bottom-20 lg:bottom-24 w-full left-0 right-0 z-10 pointer-events-none">
