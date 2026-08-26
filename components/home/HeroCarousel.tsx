bbd1da6da5d270cbf6130250afb2c094b016d9c3
mevan1771 <mevan1771@gmail.com>
2026-08-26 20:55:23 +0100

    style: reduce homepage hero darkness to match activity page

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -71,8 +71,8 @@ export function HeroCarousel({ tours, introSlide }: { tours: Tour[], introSlide?
           />
 
           {/* Slide-specific Legibility Mask */}
-          <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/5 to-transparent pointer-events-none z-10"></div>
-          <div className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-black/40 to-transparent pointer-events-none z-10"></div>
+          <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-black/40 to-transparent pointer-events-none z-10"></div>
+          <div className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-black/20 to-transparent pointer-events-none z-10"></div>
 
           {/* Slide Content */}
           <div className="absolute bottom-8 md:bottom-20 lg:bottom-24 w-full left-0 right-0 z-10 pointer-events-none">
